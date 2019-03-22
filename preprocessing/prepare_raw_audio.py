47772f30564dc52c5cc99089bb61e4313e742f94
Gustavo Cid Ornelas <gustavocidornelas@student-net-etx-1168.ethz.ch>
2019-03-22 15:49:20 +0100

    Fixed directory paths

--- preprocessing/prepare_raw_audio.py
+++ preprocessing/prepare_raw_audio.py
@@ -4,21 +4,21 @@ import csv
 if __name__ == "__main__":
 
     # output file
-    out_file = "FC_raw_audio.csv"
+    out_file = "../data/processed-data/FC_raw_audio.csv"
 
     # reading all of the ids that are going to be used
-    with open("../data/processed data/FC_ordered_ids.txt") as f:
+    with open("../data/processed-data/FC_ordered_ids.txt") as f:
         ordered_ids = f.readlines()
 
     file_count = 0
 
-    with open(out_file, "wb") as f:
+    with open(out_file, "w") as f:
         # finding the corresponding .wav files specified in ordered_ids
         for id in ordered_ids:
             current_session = id[4]
             partial_id = id[0:-6]
 
-            audio_file = "../data/raw data/IEMOCAP_full_release/Session" + current_session + "/sentences/wav/" + partial_id + "/" + id[0:-1] + ".wav"
+            audio_file = "../data/raw-data/IEMOCAP_full_release/Session" + current_session + "/sentences/wav/" + partial_id + "/" + id[0:-1] + ".wav"
 
             # reading the audio file
             fs, samples = scipy.io.wavfile.read(audio_file)
