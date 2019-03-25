063b5180d5be86d42490aa0bfa27c376f31120a6
Gustavo Cid Ornelas <gustavocidornelas@student-net-etx-1168.ethz.ch>
2019-03-25 08:22:10 +0100

    Created the parameters file

---
+++ parameters/parameters.py
@@ -0,0 +1,19 @@
+
+##################################
+# Paths
+##################################
+# TODO: add all of the relevant paths here instead of hardcoding them
+
+##################################
+# Training
+##################################
+# number of training steps
+num_training_steps = 10000
+# learning rate
+lr = 0.01
+
+##################################
+# Audio
+##################################
+
+
