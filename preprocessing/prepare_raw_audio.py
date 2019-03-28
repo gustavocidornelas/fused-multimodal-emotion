d462c37dcc1a1aed8e5fd9f43b56c6e93ceac8e1
Gustavo Cid Ornelas <gustavocidornelas@student-net-etx-1093.ethz.ch>
2019-03-28 10:56:16 +0100

    Truncated and zero padded all the audios to have the same length

--- preprocessing/prepare_raw_audio.py
+++ preprocessing/prepare_raw_audio.py
@@ -4,7 +4,10 @@ Created on Fri March 22, 2019
 @author: Gustavo Cid Ornelas
 """
 import scipy.io.wavfile
-import csv
+import numpy as np
+
+from sklearn import preprocessing
+
 
 if __name__ == "__main__":
     """
@@ -13,7 +16,7 @@ if __name__ == "__main__":
     """
 
     # output file
-    out_file = "../data/processed-data/FC_raw_audio.csv"
+    out_file = "../data/processed-data/FC_raw_audio.npy"
 
     # reading all of the ids that are going to be used
     with open("../data/processed-data/FC_ordered_ids.txt") as f:
@@ -21,25 +24,42 @@ if __name__ == "__main__":
 
     file_count = 0
 
+    # every audio should have the same length (250.000) for the batches
+    audio_data = np.zeros((len(ordered_ids), 250000))
+
     with open(out_file, "w") as f:
         # finding the corresponding .wav files specified in ordered_ids
-        for id in ordered_ids:
+        for row, id in enumerate(ordered_ids):
             current_session = id[4]
             partial_id = id[0:-6]
 
-            audio_file = "../data/raw-data/IEMOCAP_full_release/Session" + current_session + "/sentences/wav/" + partial_id + "/" + id[0:-1] + ".wav"
+            audio_file = "../data/raw-data/IEMOCAP_full_release/Session" + current_session + "/sentences/wav/" + \
+                         partial_id + "/" + id[0:-1] + ".wav"
 
             # reading the audio file
-            fs, samples = scipy.io.wavfile.read(audio_file)
+            _, samples = scipy.io.wavfile.read(audio_file)
 
-            # writing the results to the csv file
-            csv_writer = csv.writer(f)
-            csv_writer.writerow([id, samples])
+            # standardizing the audio samples to have zero mean and unit variance
+            samples = preprocessing.scale(samples.astype(float))
+
+            # zero padding the audio samples
+            if samples.shape[0] < 250000:
+                len_pad = 250000 - samples.shape[0]
+                zero_pad = np.zeros(len_pad)
+                padded_samples = np.concatenate((samples, zero_pad))
+                audio_data[row, :] = padded_samples
+            elif samples.shape[0] > 250000:
+                samples = samples[:250000]
+                audio_data[row, :] = samples
 
             file_count += 1
 
             if file_count % 100 == 0:
-                print(str(100 * file_count/len(ordered_ids)) + " % of the files read...")
+                print(str(round(100 * file_count/len(ordered_ids), 2)) + "% of the files read...")
 
     print("Done!")
+
+    # saving the padded audio data
+    np.save(out_file, audio_data)
+
 
