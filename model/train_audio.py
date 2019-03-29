38f0febb6440337910cdcc421d7b80c9e0000e52
Gustavo Cid Ornelas <gustavocidornelas@student-net-etx-1093.ethz.ch>
2019-03-29 15:24:20 +0100

    Removed useless variable audio_length

--- model/train_audio.py
+++ model/train_audio.py
@@ -16,12 +16,11 @@ if __name__ == '__main__':
     # splitting the data in training and test sets and preparing it to be fed to the model
     data_handler = ProcessDataAudio(data_path)
     train_audio_data, train_labels, test_audio_data, test_labels = data_handler.split_train_test(alpha=0.9)
-    #audio_input = data_handler.get_batches(train_audio_data, batch_size)
+    # audio_input = data_handler.get_batches(train_audio_data, batch_size)
     audio_input = train_audio_data
-    audio_length = audio_input.shape[1]
 
     # creating the model (tensorflow graph)
-    model = AudioModel(batch_size, num_categories, learning_rate, num_filters, filter_lengths, audio_length, n_pool,
+    model = AudioModel(batch_size, num_categories, learning_rate, num_filters, filter_lengths, audio_input.shape[1], n_pool,
                        encoder_size, hidden_dim, num_layers)
     model.build_graph()
 
