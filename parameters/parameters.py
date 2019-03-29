f307056e99387156d2b19e2da8aa9c8a83d8317b
Gustavo Cid Ornelas <gustavocidornelas@student-net-etx-1093.ethz.ch>
2019-03-29 15:24:51 +0100

    Updated all of the parameters in use so far

--- parameters/parameters.py
+++ parameters/parameters.py
@@ -3,17 +3,31 @@
 # Paths
 ##################################
 # TODO: add all of the relevant paths here instead of hardcoding them
+data_path = '../data/processed-data/'
+
+##################################
+# General
+##################################
+num_categories = 4
 
 ##################################
 # Training
 ##################################
+# batch size
+batch_size = 32
 # number of training steps
 num_training_steps = 10000
 # learning rate
-lr = 0.01
+learning_rate = 3e-3
 
 ##################################
 # Audio
 ##################################
-
+# number of filters in each convolutional layer
+num_filters = [4, 8]
+filter_lengths = [25, 5]
+n_pool = 200
+encoder_size = 250
+hidden_dim = 200
+num_layers = 1
 
