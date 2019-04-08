16289ef474b952281b8eb6253372e5c5ec36b46c
Gustavo Cid Ornelas <gustavocidornelas@student-net-etx-2174.ethz.ch>
2019-04-08 15:33:11 +0200

    Evaluation of the model on a validation set

---
+++ model/evaluate_audio.py
@@ -0,0 +1,24 @@
+"""
+Created on Thur April 4, 2019
+
+@author: Gustavo Cid Ornelas
+"""
+
+class EvaluationAudio:
+    """
+    Class that contains the methods to evaluate the accuracy of the audio model
+    """
+
+    def __init__(self):
+
+    def evaluate_accuracy(self, sess, model):
+        """
+        Performs the evaluation of the accuracy of the current model in the test set
+        :param sess:
+        :param model:
+        :return:
+        """
+
+
+
+
