a53a8bc22cd5da4c76c3280d3343b3c347282da3
Gustavo Cid Ornelas <gustavocidornelas@student-net-etx-2174.ethz.ch>
2019-04-08 17:02:30 +0200

    Evaluation of the model on a validation set

--- model/evaluate_audio.py
+++ model/evaluate_audio.py
@@ -1,24 +1,46 @@
 """
-Created on Thur April 4, 2019
+Created on Mon April 8, 2019
 
 @author: Gustavo Cid Ornelas
 """
 
-class EvaluationAudio:
+
+class EvaluateAudio:
     """
-    Class that contains the methods to evaluate the accuracy of the audio model
+    Class that contains the methods to evaluate the text model on the test or validation set
     """
 
-    def __init__(self):
-
-    def evaluate_accuracy(self, sess, model):
+    def evaluate_audio_model(self, sess, model, val_iterator, handle, val_handle, writer_val):
         """
-        Performs the evaluation of the accuracy of the current model in the test set
-        :param sess:
-        :param model:
-        :return:
-        """
+        Performs the evaluation of the model in the validation set
 
+        Parameters
+        ----------
+        sess: tensorflow session
+        model (TextModel object): text model
+        val_iterator (Iterator object): iterator for the validation dataset
+        handle (string): handle, to switch between the datasets
+        val_handle (string): validation dataset handle
+        eval_batch_len (int): length of the evaluation batch
+
+        Returns
+        ----------
+        val_accuracy (float): accuracy on the validation set
+        """
+        print('Evaluating on the validation set...')
+
+        # initializing the validation iterator with the validation data
+        sess.run(val_iterator.initializer)
+
+        # evaluating the model on the validation dataset
+        _, val_accuracy, val_loss, val_summary = sess.run([model.optimizer, model.accuracy, model.loss,
+                                                           model.summary_op], feed_dict={handle: val_handle})
+
+        writer_val.add_summary(val_summary, global_step=model.global_step.eval())
+
+        print('Validation accuracy: {:.4f}'.format(val_accuracy))
+
+        return val_accuracy
 
 
 
