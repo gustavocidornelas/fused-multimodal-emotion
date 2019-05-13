1be06896374874317147e39956d96aa28f4df647
gustavocidornelas <gustavocidornelas@gmail.com>
2019-05-13 13:56:26 +0200

    Created test and validation sets for the pre-trained audio model

--- model/import_model.py
+++ model/import_model.py
@@ -5,7 +5,7 @@ Created on Fri May 10, 2019
 """
 import tensorflow as tf
 
-from parameters.parameters import *
+from parameters import *
 
 
 class ImportAudioModel:
@@ -13,10 +13,16 @@ class ImportAudioModel:
     Class that restores the graph of the trained model specified in '../pretrained-models' and runs it locally to return
     the values of interest
     """
-    def __init__(self, train_audio_data, train_labels):
+    def __init__(self, train_audio_data, train_labels, test_audio_data, test_labels, val_audio_data, val_labels):
         # creating a local graph
         self.graph = tf.Graph()
         self.sess = tf.Session(graph=self.graph)
+
+        # data
+        self.test_audio_data = test_audio_data
+        self.test_labels = test_labels
+        self.val_audio_data = val_audio_data
+        self.val_labels = val_labels
 
         with self.graph.as_default():
             # restoring the audio model graph
@@ -43,8 +49,8 @@ class ImportAudioModel:
 
             # creating the audio training, testing and validation handles (to switch between datasets)
             self.train_audio_handle = self.sess.run(self.train_audio_iterator.string_handle())
-            #self.test_audio_handle = self.sess.run(test_audio_iterator.string_handle())
-            #self.val_audio_handle = self.sess.run(val_audio_iterator.string_handle())
+            self.test_audio_handle = self.sess.run(self.test_audio_iterator.string_handle())
+            self.val_audio_handle = self.sess.run(self.val_audio_iterator.string_handle())
 
     def _create_audio_datasets(self):
         """
@@ -62,9 +68,19 @@ class ImportAudioModel:
         self.train_audio_dataset = self.train_audio_dataset.batch(batch_size)
         self.train_audio_iterator = self.train_audio_dataset.make_initializable_iterator()
 
+        # test dataset
+        self.test_audio_dataset = tf.data.Dataset.from_tensor_slices((self.test_audio_data, self.test_labels))
+        self.test_audio_dataset = self.test_audio_dataset.batch(self.test_audio_data.shape[0])
+        self.test_audio_iterator = self.test_audio_dataset.make_initializable_iterator()
+
+        # validation dataset
+        self.val_audio_dataset = tf.data.Dataset.from_tensor_slices((self.val_audio_data, self.val_labels))
+        self.val_audio_dataset = self.val_audio_dataset.batch(self.val_audio_data.shape[0])
+        self.val_audio_iterator = self.val_audio_dataset.make_initializable_iterator()
+
     def run_audio_model_train(self):
         """
-        Runs a batch of the audio data through the audio model
+        Runs a batch of the train audio data through the audio model
 
         Returns
         ----------
@@ -76,6 +92,37 @@ class ImportAudioModel:
                                                               feed_dict={self.handle: self.train_audio_handle})
 
         return batch_hidden_states
+
+    def run_audio_model_test(self):
+        """
+        Runs the test audio data through the audio model
+
+        Returns
+        ----------
+        batch_hidden_states (array): array of shape [batch_size, hidden_dim, audio_rnn_steps] with the hidden states at
+                                     every time step of the audio RNN
+        """
+        print('Feeding batch to audio model...')
+        batch_prediction, batch_hidden_states = self.sess.run([self.predictions, self.hidden_states],
+                                                              feed_dict={self.handle: self.test_audio_handle})
+
+        return batch_hidden_states
+
+    def run_audio_model_val(self):
+        """
+        Runs the validation audio data through the audio model
+
+        Returns
+        ----------
+        batch_hidden_states (array): array of shape [batch_size, hidden_dim, audio_rnn_steps] with the hidden states at
+                                     every time step of the audio RNN
+        """
+        print('Feeding batch to audio model...')
+        batch_prediction, batch_hidden_states = self.sess.run([self.predictions, self.hidden_states],
+                                                              feed_dict={self.handle: self.val_audio_handle})
+
+        return batch_hidden_states
+
 
 
 
